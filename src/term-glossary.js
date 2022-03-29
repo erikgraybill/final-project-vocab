42f181fd8bbadb1af1eec4fbb5190a6ba99a428c
ParkerEM <eparker00@pm.me>
2022-03-29 17:08:36 -0400

    glossary update

--- src/term-glossary.js
+++ src/term-glossary.js
@@ -10,33 +10,79 @@ export class TermGlossary extends LitElement {
     static get properties() {
         return {
             term: {},
+            definition: {},
+            links: {},
+            endpoint: {},
+            glossary: {},
         }
     }
 
     constructor() {
         super();
         this.term = "";
+        this.definition = "";
+        this.links = [];
+        this.endpoint = "";
+        this.glossary = [];
+    }
+
+    async addWord() {
+        html` 
+        <vocab-term>
+            <details>
+                <summary>${this.term}</summary>
+                <p slot="information">${this.definition}</p>
+                <ul class="links">
+                    <!-- <li><a href="${this.links}">Link to information</a></li> -->
+                </ul>
+            </details>
+        </vocab-term>
+        `
     }
 
     async getData() {
-        fetch();
+        fetch('').then(resp => {
+            if (resp.ok) {
+              return resp.json();
+            }
+            return false;
+        }).then(data => {
+            this.glossary = [];
+            for (let i = 0; i < data.collection.items.length; i++) {
+                const results = {
+                    term: data.collection.items[i].data[0].summary,
+                    definition: data.collection.items[i].data[0].information,
+                    links: data.collection.items[i].data[0].links,
+                };
+                this.glossary.push(results);
+            }
+        });
     }
 
     async searchData() {
-        fetch();
+        fetch('').then();
     }
+
+    updated(changedProperties) {
+		changedProperties.forEach((old, propName) => {
+			if (propName === '') {
+				this.getData(this[propName]); 
+			}
+		});
+	}
 
     render() {
         return html`
-            <vocab-term>
-                <details>
-                <summary>Coffee</summary>
-                <p slot="information">Bean juice made into warm beverage.</p>
-                <ul class="links">
-                    <li><a href="https://www.starbucks.com/">Link to starbucks information</a></li>
-                </ul>
-                </details>
-            </vocab-term>
+            <h1>Glossary</h1>
+            <dl>
+                ${this.glossary.map(
+                    item => html`
+                        <dt>${this.term}</dt>
+                        <dd>${this.definition}</dd>
+                        <dd>${this.links}</dd>
+                    `
+                )}
+            </dl>
         `;
     }
 }
