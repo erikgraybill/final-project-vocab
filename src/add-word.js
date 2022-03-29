fc9b6aa4950cc0ee412038dc5bf309018214a7b9
Erik <graybiller@gmail.com>
2022-03-29 12:50:19 -0400

    backend update

--- src/add-word.js
+++ src/add-word.js
@@ -6,19 +6,37 @@ export class AddWordForm extends LitElement {
     }
 
     static get properties() {
-        return {
-            term: {},
-        }
-    }
+		return {
+			endpoint: { type: String },
+			term: { type: String },
+            def: { type: String },
+            links: { type: Array }
+		}
+	}
 
-    constructor() {
-        super();
-        this.term = "";
+	constructor() {
+		super();
+        this.endpoint = '/api/vocab';
+		this.term = '';
+        this.def = '';
+        this.links = [];
+	}
+
+    async getData() {
+        
     }
 
     render() {
         return html`
-            <>
+            <vocab-term>
+                <details>
+                <summary>Coffee</summary>
+                <p slot="information">Bean juice made into warm beverage.</p>
+                <ul class="links">
+                    <li><a href="https://www.starbucks.com/">Link to starbucks information</a></li>
+                </ul>
+                </details>
+            </vocab-term>
         `;
     }
 }
